a1c0786373368bcdc9848be86ac246e560a53a57
Wizkaley <kaleyeshan@gmail.com>
2019-09-09 18:58:24 +0530

    Bucket Set Up in BoltDB

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"task/db"
+	"path/filepath"
 	"task/cmd"
 	//"github.com/spf13/cobra"
+	"github.com/mitchellh/go-homedir"
 )
 
 func main(){
+	home,_ := homedir.Dir()
+	dbPath := filepath.Join(home,"tasks.db")
+	err := db.Init(dbPath); if err!=nil{
+		panic(err)
+	}
+	fmt.Println("DB Conn")
 	cmd.RootCmd.Execute()
 }
